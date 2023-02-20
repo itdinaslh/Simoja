b981836c68e4073efeb41fc3cfe348e186138fc4
itdinaslh <itdlh@outlook.com>
2023-02-20 17:47:43 +0700

    Update Public Web

--- Controllers/WebController.cs
+++ Controllers/WebController.cs
@@ -6,5 +6,23 @@ public class WebController : Controller {
     [HttpGet("/")]
     public IActionResult Index() => View();
 
-    
+    [HttpGet("/web/usaha-kegiatan")]
+    public IActionResult Usaha() => View();
+
+    [HttpGet("/web/penyedia-jasa")]
+    public IActionResult PenyediaJasa() => View();
+
+
+    [HttpGet("/web/jasa-pengangkutan-sampah")]
+    public IActionResult JasaAngkut() => View();
+
+    [HttpGet("/web/jasa-pengolahan-sampah")]
+    public IActionResult JasaOlah() => View();
+
+
+    [HttpGet("/web/kinerjaps")]
+    public IActionResult Kinerjaps() => View();
+
+    [HttpGet("/web/kontak")]
+    public IActionResult Kontak() => View();
 }
