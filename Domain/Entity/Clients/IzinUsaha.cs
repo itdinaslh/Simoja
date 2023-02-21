30a9d96daba925aab45cf7666b141b738718d617
itdinaslh <itdlh@outlook.com>
2023-02-22 00:49:01 +0700

    Update Sidebar

---
+++ Domain/Entity/Clients/IzinUsaha.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Simoja.Entity;
+
+public class IzinUsaha
+{
+	[Key]
+	public Guid IzinUsahaID { get; set; }
+
+	public Guid ClientID { get; set; }
+
+	[Required(ErrorMessage = "")]
+	public int JenisKegiatanID { get; set; }
+}
